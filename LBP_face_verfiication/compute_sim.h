323e4517fece0f10f022fe8b111d4df672998d64
ImSKH <75467378+skihyeon@users.noreply.github.com>
2023-08-31 01:47:23 +0900

    Delete LBP_face_verfiication directory

--- LBP_face_verfiication/compute_sim.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COMPUTE_SIM_H__
-#define __COMPUTE_SIM_H__
-
-#include <stdio.h>
-#include <math.h>
-
-float compute_sim(float* ref, float* tar);
-
-#endif
--- LBP_face_verfiication/main.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "LBP_lookup.h"
-#include "compute_sim.h"
-#include "LBP_Histo.h"
-#include "LBP.h"
