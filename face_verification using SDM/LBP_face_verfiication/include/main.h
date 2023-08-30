f46cd0c84792c333263efc2bb434a3c3dcea973d
ImSKH <75467378+skihyeon@users.noreply.github.com>
2023-08-31 01:47:30 +0900

    Delete face_verification using SDM directory

--- face_verification using SDM/LBP_face_verfiication/include/main.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "LBP_lookup.h"
-#include "compute_sim.h"
-#include "LBP_Histo.h"
-#include "LBP.h"
-#include "LBP_landmark.h"
-#include "LBP_landmark_histo.h"
-//#include "get_ref_capture.h"
--- face_verification using SDM/get_ref_capture.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GET_REF_CAPTURE_H__
-#define __GET_REF_CAPTURE_H__
-
-int ref_exist();
-int get_ref_capture(Mat frame);
-
-#endif
